a21afc14f46d572da607fb46a647bdd3ac8afc50
holabayor <liasu.olabayo@gmail.com>
2022-06-02 14:29:02 +0100

    fixed task 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,6 +5,6 @@ if __name__ == "__main__":
     if n == 1:
         print("0 arguments.")
     else:
-        print(f"{n-1} arguments:")
+        print(f"{n-1} arguments:" if n > 2 else f"{n-1} argument:")
     for i in range(1, n):
         print(f"{i}: {sys.argv[i]}")
