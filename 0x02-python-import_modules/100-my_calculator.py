15b970f737c6e98175d58a2688c7a79fafd7a069
holabayor <liasu.olabayo@gmail.com>
2022-06-02 17:04:27 +0100

    task 100

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,6 +1,17 @@
 #!/usr/bin/python3
+import sys
+from calculator_1 import add, sub, mul, div
 
-def calculate(a, op, b):
+
+def main():
+    if len(sys.argv) < 4:
+        print(f"Usage: {sys.argv[0]} <a> <operator> <b>")
+        exit(1)
+
+    a = int(sys.argv[1])
+    op = sys.argv[2]
+    b = int(sys.argv[3])
+
     if op == '+':
         print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
     elif op == '-':
@@ -10,20 +21,9 @@ def calculate(a, op, b):
     elif op == '/':
         print("{:d} / {:d} = {:d}".format(a, b, div(a, b)))
     else:
-        print("Unknown operator. Available operator: +, -, * and /")
+        print("Unknown operator. Available operators: +, -, * and /")
         exit(1)
 
 
 if __name__ == "__main__":
-    import sys
-    from calculator_1 import add, sub, mul, div
-
-    if len(sys.argv) < 4:
-        print(f"Usage: {sys.argv[0]} <a> <operator> <b>")
-        exit(1)
-
-    a = int(sys.argv[1])
-    op = sys.argv[2]
-    b = int(sys.argv[3])
-
-    calculate(a, op, b)
+    main()
