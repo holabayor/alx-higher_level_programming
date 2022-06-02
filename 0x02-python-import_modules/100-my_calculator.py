eed5ac4fe3354d408e57fd87b215e13b3bc5fd9e
holabayor <liasu.olabayo@gmail.com>
2022-06-02 15:04:51 +0100

    task 100

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+
+def calculate(a, op, b):
+    if op == '+':
+        print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
+    elif op == '-':
+        print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
+    elif op == '':
+        print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
+    elif op == '/':
+        print("{:d} / {:d} = {:d}".format(a, b, div(a, b)))
+    else:
+        print("Unknown operator. Available operator: +, -, * and /")
+        exit(1)
+
+if __name__ == "__main__":
+    import sys
+    from calculator_1 import add, sub, mul, div
+    
+    if len(sys.argv) < 4:
+        print(f"Usage: {sys.argv[0]} <a> <operator> <b>")
+        exit(1)
+
+    a = int(sys.argv[1])
+    op = sys.argv[2]
+    b = int(sys.argv[3])
+    
+    calculate(a, op, b)
+
