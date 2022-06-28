fbd3e1ee214f6748ece6a254d78f0521b7433cb1
holabayor <liasu.olabayo@gmail.com>
2022-06-28 01:24:47 +0100

    fixed sum method

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
-
-
 """Define a class rectangle"""
 
 
@@ -19,10 +17,12 @@ class Rectangle:
 
     @property
     def width(self):
+        """Getter for the rectangle width"""
         return self.__width
 
     @width.setter
     def width(self, value):
+        """setter for the rectangle width"""
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
         elif (value < 0):
@@ -32,21 +32,15 @@ class Rectangle:
 
     @property
     def height(self):
+        """Getter method for the rectangle height"""
         return self.__height
 
     @height.setter
     def height(self, value):
+        """setter for the rectangles height"""
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
         elif (value < 0):
             raise ValueError("height must be >= 0")
         else:
             self.__height = value
-
-    def area(self):
-        return (self.__width * self.__height)
-
-    def perimeter(self):
-        if (self.__width or self.__height) == 0:
-            return 0
-        return 2 * (self.__width + self.__height)
