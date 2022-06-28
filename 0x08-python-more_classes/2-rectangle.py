704a3aa76c1808c6bedd3652d47f3326fd42ea76
holabayor <liasu.olabayo@gmail.com>
2022-06-28 01:26:11 +0100

    fixed sum method

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -44,3 +44,13 @@ class Rectangle:
             raise ValueError("height must be >= 0")
         else:
             self.__height = value
+
+    def area(self):
+        """Returns the area of the rectangle"""
+        return (self.__width * self.__height)
+
+    def perimeter(self):
+        """Returns the perimeter of the rectangle"""
+        if not (self.__width or self.__height):
+            return 0
+        return 2 * (self.__width + self.__height)
