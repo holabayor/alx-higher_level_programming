408a7c130ab40f8d79a2b7883e526c1a3ce4052c
holabayor <liasu.olabayo@gmail.com>
2022-07-05 11:33:36 +0100

    lookup

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,7 @@
+#!/usr/bin/pyhton3
+""" This module looks up the attributes of an object"""
+
+
+def lookup(obj):
+    """A function that returns the attributes and methods of a given object"""
+    return dir(obj)
