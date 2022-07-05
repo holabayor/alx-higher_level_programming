4e71c4610bc31ef4650eeb5bdc8a095821d76246
holabayor <liasu.olabayo@gmail.com>
2022-07-05 12:27:18 +0100

    class type and instances

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -4,8 +4,8 @@ This module implements a function a that checks the instance of an object
 """
 
 
-def is_same_class(obj, a_class):
+def is_kind_of_class(obj, a_class):
     """ Function that checks if an object is an instance
     of a specified class
     """
-    return type(obj) is a_class
+    return isinstance(obj, a_class)
