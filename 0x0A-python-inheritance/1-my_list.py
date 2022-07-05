5c8c8215a8a65cb724eab10b1c6313db0be2cba5
holabayor <liasu.olabayo@gmail.com>
2022-07-05 11:45:13 +0100

    print sorted list

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+This module implements a customised list object
+"""
+
+class MyList(list):
+    """
+    My ALX customised list
+    """
+    def print_sorted(self):
+        """
+        This method prints the a sorted list in ascending order
+        """
+        print(sorted(self))
