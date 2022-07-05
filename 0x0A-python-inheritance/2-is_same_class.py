a603943960cfc0a7f8b8b2ad35cead94dd71687f
holabayor <liasu.olabayo@gmail.com>
2022-07-05 12:21:15 +0100

    check for smae class

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+"""
+
+
+def is_same_class(obj, a_class):
+    """ Function that checks if an object is an instance 
+    of a specified class
+    """
+    return isinstance(obj, a_class)
