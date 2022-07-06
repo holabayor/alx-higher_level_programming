9c8fa2167d312673d283336e6ac51e9814eba768
holabayor <liasu.olabayo@gmail.com>
2022-07-06 15:24:52 +0100

    add item

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -7,8 +7,7 @@ import sys
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-items = []
-items.extend(sys.argv[1:])
 filename = "add_item.json"
+items = load_from_json_file(filename)
+items.extend(sys.argv[1:])
 save_to_json_file(items, filename)
-load_from_json_file(filename)
