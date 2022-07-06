6a331418e2a02ba6d19916c5b590eb6f52a7a641
holabayor <liasu.olabayo@gmail.com>
2022-07-06 23:54:22 +0100

    inheritance

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+This module implements a customised int object
+"""
+
+
+class MyInt(int):
+    """
+    My ALX customised int
+    """
+    def __new__(cls, *args, **kwargs):
+        """create a new instance of the class"""
+        return super(MyInt, cls).__new__(cls, *args, **kwargs)
+
+    def __eq__(self, other):
+        """what was != is now =="""
+        return int(self) != other
+
+    def __ne__(self, other):
+        """what was == is now !="""
+        return int(self) == other
