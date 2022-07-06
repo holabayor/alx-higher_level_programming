cd41b9e45a5672169accacfc48cd906e489956a4
holabayor <liasu.olabayo@gmail.com>
2022-07-06 23:31:40 +0100

    append lines after

---
+++ 0x0B-python-input_output/100-append_after.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" Search and update module
+"""
+
+
+def append_after(filename="", search_string="", new_string=""):
+    """
+    This function inserts a line to a text file after each
+    line containing a specific string
+    """
+    with open(filename, "r", encoding="utf-8") as f:
+        text = ""
+        for line in f.readlines():
+            text += line
+            if search_string in line:
+                text += new_string
+    with open(filename, "a") as f:
+        f.write(text)
