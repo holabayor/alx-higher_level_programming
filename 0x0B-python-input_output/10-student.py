aa607475193ef7ba0084a957937ef6c3fe5c37be
holabayor <liasu.olabayo@gmail.com>
2022-07-06 22:43:16 +0100

    student class update

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+""" Student module
+"""
+
+
+class Student:
+    """ Student
+    """
+    def __init__(self, first_name, last_name, age):
+        """
+        Initialization
+
+        Args:
+            first_name
+            last_name
+            age
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        json_dict = self.__dict__
+        
+        if attrs is not None and all(isinstance(x, str) for x in attrs):
+            json_dict = {}
+            try:
+                for k, v in self.__dict__.items():
+                    if k in attrs:
+                        json_dict[k] = v
+            except KeyError:
+                pass
+        return json_dict
+
