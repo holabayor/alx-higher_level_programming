7c59fc8016b2da608534dc78b28b1661d4a3668e
holabayor <liasu.olabayo@gmail.com>
2022-07-12 18:36:07 +0100

    rectangle attributes validation

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,10 +1,15 @@
 #!/usr/bin/python3
-from base import Base
+"""Rectangle object"""
+from models.base import Base
 
 
 class Rectangle(Base):
+    """ Rectangle object implementation
+    """
 
     def __init__(self, width, height, x=0, y=0, id=None):
+        """Initialization
+        """
         super().__init__(id)
 
         self.width = width
@@ -12,12 +17,26 @@ class Rectangle(Base):
         self.x = x
         self.y = y
 
+    def validate(self, name: str, value: int, not_zero=True) -> None:
+        """Validates the input values
+        """
+
+        if not isinstance(value, int):
+            raise TypeError("{} must be an integer".format(name))
+        if not_zero:
+            if value <= 0:
+                raise ValueError("{} must be > 0".format(name))
+        else:
+            if value < 0:
+                raise ValueError("{} must be >= 0".format(name))
+
     @property
     def width(self):
         return self.__width
 
     @width.setter
     def width(self, value):
+        self.validate('width', value)
         self.__width = value
 
     @property
@@ -26,6 +45,7 @@ class Rectangle(Base):
 
     @height.setter
     def height(self, value):
+        self.validate('height', value)
         self.__height = value
 
     @property
@@ -34,6 +54,7 @@ class Rectangle(Base):
 
     @width.setter
     def x(self, value):
+        self.validate('x', value, False)
         self.__x = value
 
     @property
@@ -42,4 +63,5 @@ class Rectangle(Base):
 
     @width.setter
     def y(self, value):
+        self.validate('y', value, False)
         self.__y = value
