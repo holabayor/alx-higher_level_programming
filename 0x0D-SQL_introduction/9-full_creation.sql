c3a4b75ec4bdda9d7e242ac0b818aaf3b6b4a46a
holabayor <liasu.olabayo@gmail.com>
2022-08-09 22:56:35 +0100

    mandatory tasks

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,4 +1,5 @@
 --  that creates a table second_table in the database
 --  hbtn_0c_0 in your MySQL server and add multiples rows
-INSERT INTO second_table  (`id` INT, `name` VARCHAR(256), `age` INT) VALUES 
-(1, “John”,  10), (2, “Alex”,  3), (3, “Bob”,  14), (4, “George”,  8);
+CREATE TABLE IF NOT EXISTS second_table(`id` INT, `name` VARCHAR(256), `age` INT);
+INSERT INTO second_table VALUES 
+(1, "John",  10), (2, "Alex",  3), (3, "Bob",  14), (4, "George",  8);
