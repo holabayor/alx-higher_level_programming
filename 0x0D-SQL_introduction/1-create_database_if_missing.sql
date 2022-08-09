e3e2e5d4c920534194975816e5dab8466c53ddd9
holabayor <liasu.olabayo@gmail.com>
2022-08-09 22:41:51 +0100

    mandatory tasks

---
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -0,0 +1,2 @@
+-- script to create a database into mysql server
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
---
+++ 0x0D-SQL_introduction/2-remove_database.sql
@@ -0,0 +1,2 @@
+-- script to delete a database into mysql server
+DROP DATABASE IF EXISTS hbtn_0c_0;
