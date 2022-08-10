2f4f4fbfad6df42bf33428b8f78b368a88a0cc74
holabayor <liasu.olabayo@gmail.com>
2022-08-10 21:26:39 +0100

    mandatory tasks

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,5 +1,5 @@
 --  script that creates the user user_0d_2
 --  user_0d_2 should have ALL priviledges
 CREATE DATABASE IF NOT EXISTS htbn_0d_1;
-CREATE USER IF NOT EXISTS user_0d_1@localhost IDENTIFIED BY 'user_0d_pwd';
+CREATE USER IF NOT EXISTS user_0d_1@localhost IDENTIFIED BY 'user_0d_1_pwd';
 GRANT ALL ON *.* TO user_0d_1@localhost;
