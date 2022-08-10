3d0d0294be552ec4f4793a3d32fd227168df68bc
holabayor <liasu.olabayo@gmail.com>
2022-08-10 21:23:54 +0100

    mandatory tasks

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -2,4 +2,4 @@
 --  user_0d_2 should have ALL priviledges
 CREATE DATABASE IF NOT EXISTS htbn_0d_1;
 CREATE USER IF NOT EXISTS user_0d_1@localhost IDENTIFIED BY 'user_0d_pwd';
-GRANT ALL ON *.* TO user_0d_1;
+GRANT ALL ON *.* TO user_0d_1@localhost;
--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -2,4 +2,5 @@
 --  user_0d_2 should have only SELECT priviledges
 CREATE DATABASE IF NOT EXISTS htbn_0d_2;
 CREATE USER IF NOT EXISTS user_0d_2@localhost IDENTIFIED BY 'user_0d_2_pwd';
-GRANT SELECT ON htbn_0d_2 TO user_0d_2
+GRANT SELECT ON htbn_0d_2 TO user_0d_2@localhost;
+FLUSH PRIVILEDGES;
