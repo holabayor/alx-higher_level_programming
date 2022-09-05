a987dcb835348a3a84a612ea43eef13164f3907a
holabayor <liasu.olabayo@gmail.com>
2022-09-05 17:38:10 +0100

    add function

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
-const first = process.argv[2];
-const second = process.argv[3];
+const first = parseInt(process.argv[2]);
+const second = parseInt(process.argv[3]);
 
 function add (a, b) {
   return (a + b);
