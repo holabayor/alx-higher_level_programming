7e754d723a4a3e680a3f4c31a979fb9cadb861c1
holabayor <liasu.olabayo@gmail.com>
2022-09-06 15:50:12 +0100

    square class methods

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -7,7 +7,7 @@ module.exports = class Square extends SquareModel {
     super(size, size);
   }
 
-  charPrint (c='X') {
+  charPrint (c = 'X') {
     for (let i = 0; i < this.height; i++) {
       console.log(c.repeat(this.width));
     }
