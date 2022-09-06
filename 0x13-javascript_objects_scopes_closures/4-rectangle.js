a012c0d0f7a2398140185a47eaac9a47e99a1e32
holabayor <liasu.olabayo@gmail.com>
2022-09-06 15:09:51 +0100

    rectangle class methods

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -20,7 +20,7 @@ module.exports = class Rectangle {
   }
 
   double () {
-    this.width *= this.width;
-    this.height *= this.height;
+    this.width = this.width * 2;
+    this.height = this.height * 2;
   }
 };
