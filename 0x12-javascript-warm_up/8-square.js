8b405045ad3dec578363c6019ed9cfbf28d82928
holabayor <liasu.olabayo@gmail.com>
2022-09-07 18:53:50 +0100

    square class

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -2,11 +2,7 @@
 // Script that prints a square
 const len = process.argv[2];
 
-if (typeof (args) !== Number) {
-  console.log('Missing size');
-}
+if (!parseInt(len)) console.log('Missing size');
 for (let i = 0; i < len; i++) {
-  for (let j = 0; j < len; j++) {
-    console.log('X');
-  }
+  console.log('X'.repeat(len));
 }
