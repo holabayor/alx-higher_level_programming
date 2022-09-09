44eb9e4d741288e5db9a6999ef5e8b9e114b95ec
holabayor <liasu.olabayo@gmail.com>
2022-09-09 15:29:28 +0100

    all cities by states

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -15,6 +15,7 @@ if __name__ == "__main__":
     cursor = db.cursor()
     cursor.execute("SELECT cities.id, cities.name, states.name\
                     FROM cities JOIN states ON cities.state_id = states.id\
+                    WHERE states.name LIKE BINARY '{sys.argv[4]}'\
                     ORDER BY cities.id ASC")
 
     states = cursor.fetchall()
