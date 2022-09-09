3ec51d7cee34f3c1ccf0f4b531365af3e1584fe3
holabayor <liasu.olabayo@gmail.com>
2022-09-09 15:34:00 +0100

    all cities by states

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -13,7 +13,7 @@ if __name__ == "__main__":
                          host='localhost',
                          port=3306)
     cursor = db.cursor()
-    cursor.execute("SELECT cities.id, cities.name, states.name\
+    cursor.execute("SELECT cities.name\
                     FROM cities JOIN states ON cities.state_id = states.id\
                     WHERE states.name LIKE BINARY '{sys.argv[4]}'\
                     ORDER BY cities.id ASC")
