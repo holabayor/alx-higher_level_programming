0ad904b68bc9a8ad49d3f85e7f8b3af7cc7bb7c2
holabayor <liasu.olabayo@gmail.com>
2022-09-10 10:36:27 +0100

    filter states

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """ Script that list all states from database hbtn_0e_0_usa
-    that matches the input state
+    where name matches argument passed
 """
 import sys
 import MySQLdb
@@ -13,9 +13,8 @@ if __name__ == "__main__":
                          host='localhost',
                          port=3306)
     cursor = db.cursor()
-    cursor.execute("SELECT * FROM states\
-                    WHERE BINARY name = '%s'\
-                    ORDER BY id ASC" % sys.argv[4])
+    cursor.execute(f"SELECT id, name FROM states WHERE name\
+                    = '{sys.argv[4]}' ORDER BY id ASC")
 
     states = cursor.fetchall()
 
