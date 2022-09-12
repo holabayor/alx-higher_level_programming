3d7955af317436c682ce78f679282e940efac5ac
holabayor <liasu.olabayo@gmail.com>
2022-09-12 06:07:51 +0100

    pycodestyle fix

--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -9,19 +9,17 @@ from sqlalchemy import (create_engine)
 from sqlalchemy.orm import Session
 
 if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.
-                           format(argv[1], argv[2], argv[3]),
-                           pool_pre_ping=True)
-    Base.metadata.create_all(engine)
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
+    Session = sessionmaker(bind=engine)
+    session = Session()
 
-    session = Session(engine)
-
-    data = session.query(State).join(City).\
+    states = session.query(State).join(City).\
         order_by(State.id, City.id).all()
 
-    for row in data:
-        print("{}: {}".format(row.id, row.name))
-        for city in row.cities:
+    for state in states:
+        print("{}: {}".format(state.id, state.name))
+        for city in state.cities:
             print("\t{}: {}".format(city.id, city.name))
 
     session.commit()
