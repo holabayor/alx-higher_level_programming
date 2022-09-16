2d846bac8db7e0e40e81236d14318c2af351e151
holabayor <liasu.olabayo@gmail.com>
2022-09-16 21:46:17 +0100

    relatioship states list

--- 0x0F-python-object_relational_mapping/relationship_city.py
+++ 0x0F-python-object_relational_mapping/relationship_city.py
@@ -13,4 +13,4 @@ class City(Base):
     __tablename__ = 'cities'
     id = Column(Integer, autoincrement=True, nullable=False, primary_key=True)
     name = Column(String(128), nullable=False)
-    state_id = Column(Integer(), ForeignKey('states.id'), nullable=False)
+    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
