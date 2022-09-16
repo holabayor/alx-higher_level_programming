88fe5005366db49aadde53bd8e28a58144b03f38
holabayor <liasu.olabayo@gmail.com>
2022-09-16 22:08:17 +0100

    relatioship states list

--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -3,7 +3,7 @@
     All states via SQLAlchemy
 """
 import sys
-from relationship_state import State, Base
+from relationship_state import Base, State
 from relationship_city import City
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
@@ -11,7 +11,7 @@ from sqlalchemy.orm import sessionmaker
 if __name__ == "__main__":
     engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
                            .format(sys.argv[1], sys.argv[2], sys.argv[3]),
-                           pre_pool_ping=True)
+                           pool_pre_ping=True)
     Session = sessionmaker(bind=engine)
     session = Session()
 
