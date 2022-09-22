ccc3e17f41152924da93c30982b5e2cd0b275ec3
holabayor <liasu.olabayo@gmail.com>
2022-09-22 23:57:56 +0100

    return body

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Script that sends a JSON POST request
-curl -sIX POST --data "$2" -H 'Content-Type: application/json' "$1"
+curl -sX POST --data "$2" -H "Content-Type: application/json" "$1"
