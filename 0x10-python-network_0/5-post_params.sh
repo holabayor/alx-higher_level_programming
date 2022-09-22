6ced487aebcc791989b317f740d533861d3415c5
holabayor <liasu.olabayo@gmail.com>
2022-09-22 21:08:50 +0100

    parameters values fixed

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Script that sets key-value variable of a POST request
-curl -sX POST --data "email=test@gmail.com&subject=I%20will%20always%20be%20for%20PLD" "$1"
+curl -sX POST --data "email=test@gmail.com&subject=I%20will%20always%20be%20here%20for%20PLD" "$1"
