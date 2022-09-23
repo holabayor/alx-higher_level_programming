33b59a00da347378041066ee6c98ca0b7d8b1a1f
holabayor <liasu.olabayo@gmail.com>
2022-09-24 00:23:15 +0100

    pycodestyle fixed

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -9,7 +9,7 @@ import urllib.parse
 import sys
 
 if __name__ == '__main__':
-    data = {'email' : sys.argv[2]}
+    data = {'email': sys.argv[2]}
     data = urllib.parse.urlencode(data).encode("ascii")
     with urllib.request.urlopen(sys.argv[1], data) as response:
         body = response.read()
