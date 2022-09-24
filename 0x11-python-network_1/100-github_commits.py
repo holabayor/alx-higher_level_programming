16a04121d27728e2476696229bb9961444930763
holabayor <liasu.olabayo@gmail.com>
2022-09-24 13:44:52 +0100

    dictionary get method

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -12,6 +12,6 @@ if __name__ == '__main__':
     url = "https://api.github.com/repos/{}/{}/commits".format(sys.argv[1],
                                                               sys.argv[2])
     r = requests.get(url).json()
-    for commit in r[-10:]:
-        print("{}: {}".format(commit['sha'],
-                              commit['commit']['author']['name']))
+    for commit in r[:10]:
+        print("{}: {}".format(commit.get('sha'),
+                              commit.get('commit').get('author').get('name')))
