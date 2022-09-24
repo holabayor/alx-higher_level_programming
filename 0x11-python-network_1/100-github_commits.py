9c54668bc3318b4b01a2e9368cf2459337e3dd34
holabayor <liasu.olabayo@gmail.com>
2022-09-24 13:03:33 +0100

    last 10 commits

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+'''
+Script that list 10 commits (from the most recent to oldest)
+of a repository
+'''
+import requests
+import sys
+
+if __name__ == '__main__':
+    owner = sys.argv[1]
+    repo = sys.argv[2]
+    url = "https://api.github.com/repos/{}/{}/commits".format(sys.argv[1],
+                                                              sys.argv[2])
+    r = requests.get(url).json()
+    for commit in r[:10]:
+        print("{}: {}".format(commit['sha'],
+                              commit['commit']['author']['name']))
