820bbe5de0c1123079cc348c59fec128d4648383
holabayor <liasu.olabayo@gmail.com>
2022-10-18 18:26:37 +0100

    :fire:

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -5,5 +5,5 @@ const request = require('request');
 
 const url = process.argv[2];
 request(url, (error, response, body) => {
-  console.log('code: ', response.statusCode);
+  console.log('code: ', response && response.statusCode);
 });
